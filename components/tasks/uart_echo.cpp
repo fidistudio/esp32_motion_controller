86b07765f44bbbdcb655552852cbe59cfc7ca886
Garyhulk55 <edgarodriguezs55@gmail.com>
2026-01-20 22:39:08 -0600

    fix uart send/receive tests

--- components/tasks/uart_echo.cpp
+++ components/tasks/uart_echo.cpp
@@ -17,18 +17,20 @@ static void uart_echo_task(void *arg)
 
     ESP_LOGI(TAG, "UART echo task started");
 
-    while (1) {
+    while (1)
+    {
         int len = uart_read_bytes(
             UART_PORT,
             data,
             UART_BUF_SIZE - 1,
-            pdMS_TO_TICKS(100)
-        );
+            pdMS_TO_TICKS(100));
 
-        if (len > 0) {
+        if (len > 0)
+        {
             data[len] = '\0';
-            ESP_LOGI(TAG, "Recibido: %s", (char *)data);
+            ESP_LOGI(TAG, "Recibido: %.*s", len, data);
             uart_write_bytes(UART_PORT, (const char *)data, len);
+            uart_write_bytes(UART_PORT, "\r\n", 2);
         }
     }
 }
@@ -36,14 +38,13 @@ static void uart_echo_task(void *arg)
 /* --- Inicialización pública --- */
 void uart_echo_init(void)
 {
-    const uart_config_t uart_config = {
-        .baud_rate = 115200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_APB,
-    };
+    uart_config_t uart_config = {};
+    uart_config.baud_rate = 115200;
+    uart_config.data_bits = UART_DATA_8_BITS;
+    uart_config.parity = UART_PARITY_DISABLE;
+    uart_config.stop_bits = UART_STOP_BITS_1;
+    uart_config.flow_ctrl = UART_HW_FLOWCTRL_DISABLE;
+    uart_config.source_clk = UART_SCLK_APB;
 
     uart_driver_install(UART_PORT, UART_BUF_SIZE * 2, 0, 0, NULL, 0);
     uart_param_config(UART_PORT, &uart_config);
@@ -59,6 +60,5 @@ void uart_echo_init(void)
         4096,
         NULL,
         5,
-        NULL
-    );
+        NULL);
 }
