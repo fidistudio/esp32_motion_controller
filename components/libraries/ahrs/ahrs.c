3e3204f515f1071f96ce7ba88f8c2210ccc16b46
Fidel <jorge.martinezram@ingenieria.unam.edu>
2026-03-13 10:17:26 -0600

    Modified the library of ahrs to aproximate yaw of osciloscopio

--- components/libraries/ahrs/ahrs.c
+++ components/libraries/ahrs/ahrs.c
@@ -25,15 +25,17 @@
 //---------------------------------------------------------------------------------------------------
 // Variable definitions
 
-volatile float sampleFreq = 50;                            // 2 * proportional gain (Kp)
-volatile float beta = 0.8;                                 // 2 * proportional gain (Kp)
-volatile float q0 = 1.0f, q1 = 0.0f, q2 = 0.0f, q3 = 0.0f; // quaternion of sensor frame relative to auxiliary frame
+volatile float sampleFreq = 50; // 2 * proportional gain (Kp)
+volatile float beta = 0.1;      // 2 * proportional gain (Kp)
+volatile float
+    q0 = 1.0f,
+    q1 = 0.0f, q2 = 0.0f,
+    q3 = 0.0f; // quaternion of sensor frame relative to auxiliary frame
 
 //====================================================================================================
 // Functions
 
-void ahrs_init(float sampleFreqDef, float betaDef)
-{
+void ahrs_init(float sampleFreqDef, float betaDef) {
   sampleFreq = sampleFreqDef;
   beta = betaDef;
 }
@@ -41,17 +43,19 @@ void ahrs_init(float sampleFreqDef, float betaDef)
 //---------------------------------------------------------------------------------------------------
 // AHRS algorithm update
 
-void ahrs_update(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz)
-{
+void ahrs_update(float gx, float gy, float gz, float ax, float ay, float az,
+                 float mx, float my, float mz) {
   float recipNorm;
   float s0, s1, s2, s3;
   float qDot1, qDot2, qDot3, qDot4;
   float hx, hy;
-  float _2q0mx, _2q0my, _2q0mz, _2q1mx, _2bx, _2bz, _4bx, _4bz, _2q0, _2q1, _2q2, _2q3, _2q0q2, _2q2q3, q0q0, q0q1, q0q2, q0q3, q1q1, q1q2, q1q3, q2q2, q2q3, q3q3;
+  float _2q0mx, _2q0my, _2q0mz, _2q1mx, _2bx, _2bz, _4bx, _4bz, _2q0, _2q1,
+      _2q2, _2q3, _2q0q2, _2q2q3, q0q0, q0q1, q0q2, q0q3, q1q1, q1q2, q1q3,
+      q2q2, q2q3, q3q3;
 
-  // Use IMU algorithm if magnetometer measurement invalid (avoids NaN in magnetometer normalisation)
-  if ((mx == 0.0f) && (my == 0.0f) && (mz == 0.0f))
-  {
+  // Use IMU algorithm if magnetometer measurement invalid (avoids NaN in
+  // magnetometer normalisation)
+  if ((mx == 0.0f) && (my == 0.0f) && (mz == 0.0f)) {
     ahrs_update_imu(gx, gy, gz, ax, ay, az);
     return;
   }
@@ -62,9 +66,9 @@ void ahrs_update(float gx, float gy, float gz, float ax, float ay, float az, flo
   qDot3 = 0.5f * (q0 * gy - q1 * gz + q3 * gx);
   qDot4 = 0.5f * (q0 * gz + q1 * gy - q2 * gx);
 
-  // Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
-  if (!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f)))
-  {
+  // Compute feedback only if accelerometer measurement valid (avoids NaN in
+  // accelerometer normalisation)
+  if (!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
 
     // Normalise accelerometer measurement
     recipNorm = 1.0 / sqrt(ax * ax + ay * ay + az * az);
@@ -101,19 +105,49 @@ void ahrs_update(float gx, float gy, float gz, float ax, float ay, float az, flo
     q3q3 = q3 * q3;
 
     // Reference direction of Earth's magnetic field
-    hx = mx * q0q0 - _2q0my * q3 + _2q0mz * q2 + mx * q1q1 + _2q1 * my * q2 + _2q1 * mz * q3 - mx * q2q2 - mx * q3q3;
-    hy = _2q0mx * q3 + my * q0q0 - _2q0mz * q1 + _2q1mx * q2 - my * q1q1 + my * q2q2 + _2q2 * mz * q3 - my * q3q3;
+    hx = mx * q0q0 - _2q0my * q3 + _2q0mz * q2 + mx * q1q1 + _2q1 * my * q2 +
+         _2q1 * mz * q3 - mx * q2q2 - mx * q3q3;
+    hy = _2q0mx * q3 + my * q0q0 - _2q0mz * q1 + _2q1mx * q2 - my * q1q1 +
+         my * q2q2 + _2q2 * mz * q3 - my * q3q3;
     _2bx = sqrt(hx * hx + hy * hy);
-    _2bz = -_2q0mx * q2 + _2q0my * q1 + mz * q0q0 + _2q1mx * q3 - mz * q1q1 + _2q2 * my * q3 - mz * q2q2 + mz * q3q3;
+    _2bz = -_2q0mx * q2 + _2q0my * q1 + mz * q0q0 + _2q1mx * q3 - mz * q1q1 +
+           _2q2 * my * q3 - mz * q2q2 + mz * q3q3;
     _4bx = 2.0f * _2bx;
     _4bz = 2.0f * _2bz;
 
     // Gradient decent algorithm corrective step
-    s0 = -_2q2 * (2.0f * q1q3 - _2q0q2 - ax) + _2q1 * (2.0f * q0q1 + _2q2q3 - ay) - _2bz * q2 * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (-_2bx * q3 + _2bz * q1) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + _2bx * q2 * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
-    s1 = _2q3 * (2.0f * q1q3 - _2q0q2 - ax) + _2q0 * (2.0f * q0q1 + _2q2q3 - ay) - 4.0f * q1 * (1 - 2.0f * q1q1 - 2.0f * q2q2 - az) + _2bz * q3 * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (_2bx * q2 + _2bz * q0) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + (_2bx * q3 - _4bz * q1) * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
-    s2 = -_2q0 * (2.0f * q1q3 - _2q0q2 - ax) + _2q3 * (2.0f * q0q1 + _2q2q3 - ay) - 4.0f * q2 * (1 - 2.0f * q1q1 - 2.0f * q2q2 - az) + (-_4bx * q2 - _2bz * q0) * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (_2bx * q1 + _2bz * q3) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + (_2bx * q0 - _4bz * q2) * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
-    s3 = _2q1 * (2.0f * q1q3 - _2q0q2 - ax) + _2q2 * (2.0f * q0q1 + _2q2q3 - ay) + (-_4bx * q3 + _2bz * q1) * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (-_2bx * q0 + _2bz * q2) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + _2bx * q1 * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
-    recipNorm = 1.0 / sqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); // normalise step magnitude
+    s0 = -_2q2 * (2.0f * q1q3 - _2q0q2 - ax) +
+         _2q1 * (2.0f * q0q1 + _2q2q3 - ay) -
+         _2bz * q2 * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) +
+         (-_2bx * q3 + _2bz * q1) *
+             (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) +
+         _2bx * q2 * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
+    s1 = _2q3 * (2.0f * q1q3 - _2q0q2 - ax) +
+         _2q0 * (2.0f * q0q1 + _2q2q3 - ay) -
+         4.0f * q1 * (1 - 2.0f * q1q1 - 2.0f * q2q2 - az) +
+         _2bz * q3 * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) +
+         (_2bx * q2 + _2bz * q0) *
+             (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) +
+         (_2bx * q3 - _4bz * q1) *
+             (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
+    s2 = -_2q0 * (2.0f * q1q3 - _2q0q2 - ax) +
+         _2q3 * (2.0f * q0q1 + _2q2q3 - ay) -
+         4.0f * q2 * (1 - 2.0f * q1q1 - 2.0f * q2q2 - az) +
+         (-_4bx * q2 - _2bz * q0) *
+             (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) +
+         (_2bx * q1 + _2bz * q3) *
+             (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) +
+         (_2bx * q0 - _4bz * q2) *
+             (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
+    s3 = _2q1 * (2.0f * q1q3 - _2q0q2 - ax) +
+         _2q2 * (2.0f * q0q1 + _2q2q3 - ay) +
+         (-_4bx * q3 + _2bz * q1) *
+             (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) +
+         (-_2bx * q0 + _2bz * q2) *
+             (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) +
+         _2bx * q1 * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
+    recipNorm = 1.0 / sqrt(s0 * s0 + s1 * s1 + s2 * s2 +
+                           s3 * s3); // normalise step magnitude
     s0 *= recipNorm;
     s1 *= recipNorm;
     s2 *= recipNorm;
@@ -143,12 +177,13 @@ void ahrs_update(float gx, float gy, float gz, float ax, float ay, float az, flo
 //---------------------------------------------------------------------------------------------------
 // IMU algorithm update
 
-void ahrs_update_imu(float gx, float gy, float gz, float ax, float ay, float az)
-{
+void ahrs_update_imu(float gx, float gy, float gz, float ax, float ay,
+                     float az) {
   float recipNorm;
   float s0, s1, s2, s3;
   float qDot1, qDot2, qDot3, qDot4;
-  float _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2, _8q1, _8q2, q0q0, q1q1, q2q2, q3q3;
+  float _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2, _8q1, _8q2, q0q0, q1q1, q2q2,
+      q3q3;
 
   // Rate of change of quaternion from gyroscope
   qDot1 = 0.5f * (-q1 * gx - q2 * gy - q3 * gz);
@@ -156,9 +191,9 @@ void ahrs_update_imu(float gx, float gy, float gz, float ax, float ay, float az)
   qDot3 = 0.5f * (q0 * gy - q1 * gz + q3 * gx);
   qDot4 = 0.5f * (q0 * gz + q1 * gy - q2 * gx);
 
-  // Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
-  if (!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f)))
-  {
+  // Compute feedback only if accelerometer measurement valid (avoids NaN in
+  // accelerometer normalisation)
+  if (!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
 
     // Normalise accelerometer measurement
     recipNorm = 1.0 / sqrt(ax * ax + ay * ay + az * az);
@@ -183,10 +218,13 @@ void ahrs_update_imu(float gx, float gy, float gz, float ax, float ay, float az)
 
     // Gradient decent algorithm corrective step
     s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
-    s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * q1 - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
-    s2 = 4.0f * q0q0 * q2 + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
+    s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * q1 - _2q0 * ay - _4q1 +
+         _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
+    s2 = 4.0f * q0q0 * q2 + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 +
+         _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
     s3 = 4.0f * q1q1 * q3 - _2q1 * ax + 4.0f * q2q2 * q3 - _2q2 * ay;
-    recipNorm = 1.0 / sqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); // normalise step magnitude
+    recipNorm = 1.0 / sqrt(s0 * s0 + s1 * s1 + s2 * s2 +
+                           s3 * s3); // normalise step magnitude
     s0 *= recipNorm;
     s1 *= recipNorm;
     s2 *= recipNorm;
@@ -215,11 +253,11 @@ void ahrs_update_imu(float gx, float gy, float gz, float ax, float ay, float az)
 
 /**
  * Convert the quaternion to a vector with angle.  Reverse of the code
- * in the following link: http://www.euclideanspace.com/maths/geometry/rotations/conversions/angleToQuaternion/index.htm
+ * in the following link:
+ * http://www.euclideanspace.com/maths/geometry/rotations/conversions/angleToQuaternion/index.htm
  * @return {object} Normalised vector - {x, y, z, angle}
  */
-void MadgwickGetVector(float *angle, float *x, float *y, float *z)
-{
+void MadgwickGetVector(float *angle, float *x, float *y, float *z) {
   float ang = 2.0 * acos(q0);
   float sin_angle = sin(ang / 2.0);
   *angle = ang;
@@ -228,11 +266,9 @@ void MadgwickGetVector(float *angle, float *x, float *y, float *z)
   *z = q3 / sin_angle;
 };
 
-float norm_angle_0_2pi(float a)
-{
+float norm_angle_0_2pi(float a) {
   a = fmod(a, M_PI * 2.0);
-  if (a < 0)
-  {
+  if (a < 0) {
     a += M_PI * 2.0;
   }
   return a;
@@ -250,13 +286,13 @@ float norm_angle_0_2pi(float a)
  *   https://github.com/PenguPilot/PenguPilot/blob/master/autopilot/service/util/math/quat.c#L103
  * @return {object} {heading, pitch, roll} in radians
  */
-void MadgwickGetEulerAngles(float *heading, float *pitch, float *roll)
-{
+void MadgwickGetEulerAngles(float *heading, float *pitch, float *roll) {
   float ww = q0 * q0;
   float xx = q1 * q1;
   float yy = q2 * q2;
   float zz = q3 * q3;
-  *heading = norm_angle_0_2pi(atan2f(2.0 * (q1 * q2 + q3 * q0), xx - yy - zz + ww));
+  *heading =
+      norm_angle_0_2pi(atan2f(2.0 * (q1 * q2 + q3 * q0), xx - yy - zz + ww));
   *pitch = asinf(-2.0 * (q1 * q3 - q2 * q0));
   *roll = atan2(2.0 * (q2 * q3 + q1 * q0), -xx - yy + zz + ww);
 }
@@ -274,8 +310,7 @@ void MadgwickGetEulerAngles(float *heading, float *pitch, float *roll)
  * @return {object} {heading, pitch, roll} in radians
  */
 #define RAD_2_DEG (180.0f / M_PI)
-void ahrs_get_euler_in_degrees(float *heading, float *pitch, float *roll)
-{
+void ahrs_get_euler_in_degrees(float *heading, float *pitch, float *roll) {
   MadgwickGetEulerAngles(heading, pitch, roll);
 
   *heading *= RAD_2_DEG;
